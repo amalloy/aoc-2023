fb1904e16b54c2d5e5b14543585d46ae6ff7f8a5
Alan Malloy <alan@malloys.org>
2023-12-01 03:42:35 -0800

    Day 1, part 2

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -5,17 +5,31 @@ module Main where
 
 import Control.Arrow ((&&&))
 
-import Data.Char (isDigit, digitToInt)
+import Data.Char (isDigit, digitToInt, intToDigit)
+import Data.List (isPrefixOf, tails)
+import Data.Maybe (mapMaybe)
+import Data.Monoid (First(..))
 
 type Input = [String]
 
-part1 :: Input -> Int
-part1 = sum . map calibrationValue
-  where calibrationValue = ((+) <$> ((* 10) . head) <*> last)
-          . map digitToInt . filter isDigit
+calibrationValue :: [Int] -> Int
+calibrationValue = ((+) <$> ((* 10) . head) <*> last)
 
-part2 :: Input -> ()
-part2 = const ()
+part1 :: Input -> Int
+part1 = sum . map (calibrationValue . map digitToInt . filter isDigit)
+
+digits :: [(String, Int)]
+digits = [ ("one", 1), ("two", 2), ("three", 3), ("four", 4)
+         , ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
+         ] <>
+         [(pure (intToDigit x), x) | x <- [1..9]]
+
+part2 :: Input -> Int
+part2 = sum . map (calibrationValue . (mapMaybe decodeDigit . tails))
+  where decodeDigit :: String -> Maybe Int
+        decodeDigit s = getFirst . mconcat . map (First . find) $ digits
+          where find (k, v) | k `isPrefixOf` s = Just v
+                            | otherwise = Nothing
 
 prepare :: String -> Input
 prepare = lines
