6c137bf0fd5e3d627607e00b5aaf644ccbde7cb2
Alan Malloy <alan@malloys.org>
2023-12-01 03:09:38 -0800

    Day 1, part 1

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -5,10 +5,14 @@ module Main where
 
 import Control.Arrow ((&&&))
 
+import Data.Char (isDigit, digitToInt)
+
 type Input = [String]
 
-part1 :: Input -> ()
-part1 = const ()
+part1 :: Input -> Int
+part1 = sum . map calibrationValue
+  where calibrationValue = ((+) <$> ((* 10) . head) <*> last)
+          . map digitToInt . filter isDigit
 
 part2 :: Input -> ()
 part2 = const ()
@@ -18,4 +22,3 @@ prepare = lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
-
