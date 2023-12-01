0b8f023786d6c360462dc65617eca9f4cea68dce
Alan Malloy <alan@malloys.org>
2023-12-01 07:39:12 -0800

    Move single-use values into where clauses

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -12,24 +12,15 @@ import Data.Monoid (First(..))
 
 type Input = [String]
 
-calibrationValue :: [Int] -> Int
-calibrationValue = ((+) <$> ((* 10) . head) <*> last)
-
-guardedBy :: (a -> b) -> (a -> Bool) -> a -> Maybe b
-guardedBy f p x | p x = Just $ f x
-                | otherwise = Nothing
-
 solve :: (b -> Maybe Int) -> (a -> [b]) -> [a] -> Int
 solve f g = sum . map (calibrationValue . mapMaybe f . g)
+  where calibrationValue = ((+) <$> ((* 10) . head) <*> last)
 
 part1 :: Input -> Int
 part1 = solve (digitToInt `guardedBy` isDigit) id
-
-digits :: [(String, Int)]
-digits = [ ("one", 1), ("two", 2), ("three", 3), ("four", 4)
-         , ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
-         ] <>
-         [(pure (intToDigit x), x) | x <- [1..9]]
+  where guardedBy :: (a -> b) -> (a -> Bool) -> a -> Maybe b
+        guardedBy f p x | p x = Just $ f x
+                        | otherwise = Nothing
 
 part2 :: Input -> Int
 part2 = solve decodeDigit tails
@@ -37,6 +28,11 @@ part2 = solve decodeDigit tails
         decodeDigit s = getFirst . mconcat . map (First . find) $ digits
           where find (k, v) | k `isPrefixOf` s = Just v
                             | otherwise = Nothing
+        digits = [ ("one", 1), ("two", 2), ("three", 3), ("four", 4)
+                 , ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
+                 ]
+                 <>
+                 [(pure (intToDigit x), x) | x <- [1..9]]
 
 prepare :: String -> Input
 prepare = lines
