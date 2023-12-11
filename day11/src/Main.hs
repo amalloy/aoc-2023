8e6a0445acb8fe9e04c6984ddec10780270379c1
Alan Malloy <alan@malloys.org>
2023-12-11 06:14:52 -0800

    Day 11, part 2

--- day11/src/Main.hs
+++ day11/src/Main.hs
@@ -17,8 +17,8 @@ census :: S.Set Coord -> Census
 census = foldMap go
   where go (Coord y x) = (S.singleton y, S.singleton x)
 
-part1 :: Input -> Int
-part1 u = (`div` 2) . getSum . foldMap (uncurry distance) $ S.cartesianProduct u u
+solve :: Int -> Input -> Int
+solve factor u = (`div` 2) . getSum . foldMap (uncurry distance) $ S.cartesianProduct u u
   where distance (Coord y1 x1) (Coord y2 x2) =
           foldMap Sum [ abs (y1 - y2)
                       , abs (x1 - x2)
@@ -26,12 +26,15 @@ part1 u = (`div` 2) . getSum . foldMap (uncurry distance) $ S.cartesianProduct u
                       , between (snd c) (max x1 x2) (min x1 x2)
                       ]
           where c = census u
-                between s hi lo = unpopBelow hi - unpopBelow lo
+                between s hi lo = (factor - 1) * (unpopBelow hi - unpopBelow lo)
                   where unpopBelow n = let (below, _) = S.split n s
                                        in n - S.size below
 
-part2 :: Input -> ()
-part2 = const ()
+part1 :: Input -> Int
+part1 = solve 2
+
+part2 :: Input -> Int
+part2 = solve 1000000
 
 prepare :: String -> Input
 prepare text = S.fromList $ do
