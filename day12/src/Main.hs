41a6967a5fea641f9bbf759119462498c640ba28
Alan Malloy <alan@malloys.org>
2023-12-11 23:29:47 -0800

    Day 12, part 1

--- day12/src/Main.hs
+++ day12/src/Main.hs
@@ -4,18 +4,25 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.List (group)
 
-type Input = [String]
+type Input = [(String, [Int])]
 
-part1 :: Input -> ()
-part1 = const ()
+part1 :: Input -> Int
+part1 = sum . map possibilities
+  where possibilities (s, lab) = length . filter (== lab) . map groups . traverse options $ s
+        options '?' = ".#"
+        options x = pure x
+        groups = map length . filter ((== '#') . head) . group
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = map parse . lines
+  where parse s = case break (== ' ') s of
+          (springs, (' ':label)) -> (springs, read $ ("[" <> label <> "]"))
+          _ -> error "no parse"
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
-
