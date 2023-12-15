b9c5ec1bb92cdbd79a7b1f75b129138339490e9d
Alan Malloy <alan@malloys.org>
2023-12-14 21:27:31 -0800

    Day 15, part 1

--- day15/src/Main.hs
+++ day15/src/Main.hs
@@ -1,21 +1,25 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
 
+import Data.Char (ord)
+import Data.List (foldl')
+import Data.List.Split (splitOn)
+
 type Input = [String]
 
-part1 :: Input -> ()
-part1 = const ()
+hashString :: String -> Int
+hashString = foldl' hash 0
+  where hash current c = ((current + ord c) * 17) `mod` 256
+
+part1 :: Input -> Int
+part1 = sum . map hashString
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = splitOn "," . filter (/= '\n')
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
-
