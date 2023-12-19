57cdf6db0f1ee60fec4deb9e542433561dad1248
Alan Malloy <alan@malloys.org>
2023-12-18 23:02:08 -0800

    Day 18, part 2

--- day18/src/Main.hs
+++ day18/src/Main.hs
@@ -1,72 +1,65 @@
-{-# LANGUAGE DerivingVia #-}
-
 module Main where
 
-import GHC.Generics (Generic, Generically(..))
-
 import Control.Arrow ((&&&))
-import Control.Monad (replicateM, foldM_)
-import Control.Monad.Trans.Writer (execWriter, tell)
-import Data.Coerce (coerce)
+import Control.Monad (replicateM)
 import Data.Maybe (fromMaybe)
-import Data.Semigroup (Last(..), Min(..), Max(..))
-import Linear.Vector (unit, basis)
+import Linear.Vector (unit)
 import Linear.V2 (V2(..), _x, _y)
 
 import Text.Regex.Applicative ((=~), some, anySym, sym, string, asum)
 import Text.Regex.Applicative.Common (decimal)
 
-import Data.Map.Strict qualified as M
-import Data.Set qualified as S
-
 type Direction = V2 Int
 type Color = String
-data Order = Order Direction Int Color deriving Show
-data Bounds = Bounds (V2 (Min Int)) (V2 (Max Int))
-  deriving (Show, Generic)
-  deriving Semigroup via Generically Bounds
-
-type Trench f = M.Map (V2 Int) (f Color)
-
-digTrench :: (Applicative f, Semigroup (f Color)) => [Order] -> Trench f
-digTrench = M.fromListWith (<>) . execWriter . foldM_ go (V2 0 0)
-  where go pos (Order dir len color) = do
-          tell [ (pos + dir * pure n, pure color) | n <- [0..len]]
-          pure $ pos + dir * pure len
-
-bounds :: Trench f -> Bounds
-bounds = fromMaybe (Bounds 0 0) . foldMap go . M.keys
-  where go c = Just $ Bounds (coerce c) (coerce c)
-
+data Segment = Segment Direction Int deriving Show
+data Order = Order Segment Color deriving Show
 type Input = [Order]
 
-part1 :: Input -> Int
-part1 = lavaArea . digTrench @Last
-  where lavaArea t = landArea - S.size nonLavaArea
-          where (Bounds (V2 (Min minY) (Min minX)) (V2 (Max maxY) (Max maxX))) = bounds t
-                landArea = (maxY - minY + 1) * (maxX - minX + 1)
-                nonLavaArea = go S.empty . concat $ [west, north, east, south]
-                  where xs = [minX..maxX]
-                        ys = [minY..maxY]
-                        west = flip V2 minX <$> ys
-                        east = flip V2 maxX <$> ys
-                        north = V2 minY <$> xs
-                        south = V2 maxY <$> xs
-                go seen [] = seen
-                go seen (c:cs) | S.member c seen || M.member c t = prune
-                               | otherwise = case c of
-                                   V2 y x | y < minY || y > maxY || x < minX || x > maxX -> prune
-                                          | otherwise -> go (S.insert c seen) ((fmap (c +) $ [negate, id] <*> basis) <> cs)
-                  where prune = go seen cs
+-- Both of the below polynomial calcuiations assume the last vertex is the same as the first
 
-part2 :: Input -> ()
-part2 = const ()
+-- Using "shoelace theorem" from https://en.wikipedia.org/wiki/Shoelace_formula
+polynomialArea :: [V2 Int] -> Int
+polynomialArea = (`div` 2) . abs . sum . (zipWith det <*> tail)
+  where det (V2 y1 x1) (V2 y2 x2) = x1 * y2 - x2 * y1
+
+-- The number of points contained within the polygon, including those on the
+-- perimeter and interior points (using https://en.wikipedia.org/wiki/Pick%27s_theorem)
+includedPoints :: [V2 Int] -> Int
+includedPoints vs = interior + boundary
+  where interior = area - (boundary `div` 2) + 1
+        area = polynomialArea vs
+        boundary = sum . (zipWith distance <*> tail) $ vs
+        distance c1 c2 = abs . sum $ (c2 - c1)
+
+vertices :: [Segment] -> [V2 Int]
+vertices = scanl (+) 0 . map toVec
+  where toVec (Segment dir len) = dir * pure len
+
+solve :: (Order -> Segment) -> Input -> Int
+solve f = includedPoints . vertices . map f
+
+part1 :: Input -> Int
+part1 = solve asWritten
+  where asWritten (Order seg _c) = seg
+
+part2 :: Input -> Int
+part2 = solve colorSwap
+  where colorSwap (Order _seg c) = Segment dir len
+          where (mag, [d]) = splitAt 5 c
+                dir = case d of
+                  '0' -> unit _x
+                  '1' -> unit _y
+                  '2' -> - unit _x
+                  '3' -> - unit _y
+                  x -> error $ "unknown direction " <> show x
+                len = read ("0x" <> mag)
 
 prepare :: String -> Input
 prepare = fromMaybe (error "no parse") . (=~ input)
   where input = some (order <* sym '\n')
-        order = Order <$> direction <* sym ' ' <*> decimal <* sym ' ' <*> color
+        order = Order <$> segment <* sym ' ' <*> color
         color = string "(#" *> replicateM 6 anySym <* sym ')'
+        segment = Segment <$> direction <* sym ' ' <*> decimal
         direction = asum [ (- unit _y) <$ sym 'U'
                          , (- unit _x) <$ sym 'L'
                          , unit _y <$ sym 'D'
